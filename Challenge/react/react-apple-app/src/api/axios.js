3d992fca092b00cb610ee37b42007554bfd3bbb1
h22jul22 <heeju9402@gmail.com>
2024-05-23 23:04:27 +0900

    dotenv 적용

--- Challenge/react/react-apple-app/src/api/axios.js
+++ Challenge/react/react-apple-app/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
     params: {
-        api_key: '428840d8230e52d43fbf2e2bfafe8f20',
+        api_key: import.meta.env.VITE_TMDB_ACCESS_KEY,
         language: 'ko-KR',
     },
 });
